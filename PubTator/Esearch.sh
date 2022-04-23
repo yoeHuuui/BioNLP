e95ec1c351d1561cbb42d225e930aca3982c580c
yoeHuuui <99573443+yoeHuuui@users.noreply.github.com>
2022-04-23 14:54:33 +0800

    Update Esearch.sh

--- PubTator/Esearch.sh
+++ PubTator/Esearch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=Autism&RetMax=3000" > Autism.txt
+curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=Autism&RetMax=100000" > Autism.txt
 cat Autism.txt | grep "<Id>" | sed  's/\t<Id>//g' | sed 's/<\/Id>//g' > pmid.txt
