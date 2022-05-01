d7b69669c12d45385df797de994d151ec12d31fb
yoeHuuui <99573443+yoeHuuui@users.noreply.github.com>
2022-05-01 17:23:58 +0800

    Delete Esearch.sh

--- PubTator/Esearch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=Autism&RetMax=100000" > Autism.txt
-cat Autism.txt | grep "<Id>" | sed  's/\t<Id>//g' | sed 's/<\/Id>//g' > pmid.txt
